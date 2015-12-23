47fd39839b7d36961270b1395e721604e52b3d80
Roman Vasilyev <roman@lonje.com>
2015-12-23 11:55:33 -0800

    user name can not exist

--- tracking/admin.py
+++ tracking/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from tracking.models import Visitor, VisitorManager, BannedIP, UntrackedUserAgent
 
 class VisitorAdmin(admin.ModelAdmin):
-    search_fields = ['username']
+    search_fields = ['ip_address']
 admin.site.register(Visitor, VisitorAdmin)
 admin.site.register(BannedIP)
 admin.site.register(UntrackedUserAgent)
