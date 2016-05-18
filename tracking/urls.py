b806b92d40954710fc2802c6c3f59120ec9e2cad
Roman Vasilyev <roman@lonje.com>
2016-05-18 00:58:11 -0700

    django 1.10 fix

--- tracking/urls.py
+++ tracking/urls.py
@@ -1,16 +1,16 @@
 try:
-    from django.conf.urls import patterns, url, include
+    from django.conf.urls import url, include
 except ImportError:  # Pre-Django 1.4 version
     from django.conf.urls.defaults import patterns, url, include
 from django.conf import settings
 from tracking import views
 
-urlpatterns = patterns('',
+urlpatterns = (
     url(r'^refresh/$', views.update_active_users, name='tracking-refresh-active-users'),
     url(r'^refresh/json/$', views.get_active_users, name='tracking-get-active-users'),
 )
 
 if getattr(settings, 'TRACKING_USE_GEOIP', False):
-    urlpatterns += patterns('',
+    urlpatterns += (
         url(r'^map/$', views.display_map, name='tracking-visitor-map'),
     )
